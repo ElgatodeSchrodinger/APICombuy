4fe26075b8aecd89db9158251dcf62270d46c98c
JavierT-01 <javier.quintana@unmsm.edu.pe>
2018-04-17 13:11:31 -0500

    Modelo LocalNegocio

--- localnegocio/models.py
+++ localnegocio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class localnegocio(models.Model):
+	idNegocio=models.IntegerField(primary_key=True)
+	latitud=models.FloatField()
+	longitud=models.FloatField()
+	descripcion=models.TextField()
+
+	class Meta:
+		ordering=("idNegocio",)
